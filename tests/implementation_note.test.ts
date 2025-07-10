e76fd51a60b1379851353b18a0fb75395efd16d9
Philip Schmid <philip.schmid@tum.de>
2025-07-10 22:32:02 +0200

    Reenable integration test

--- tests/implementation_note.test.ts
+++ tests/implementation_note.test.ts
@@ -6,7 +6,7 @@ import * as fs from "fs";
 import { SiomeConverter } from "../src/converter";
 
 const load_ws_sweets = () => {
-    const file = fs.readFileSync("./tests/resources/WS.Sweets.NodeSet2.xml");
+    const file = fs.readFileSync("./tests/resources/Opc.Ua.WSBasis.NodeSet2.xml");
     const dom = new JSDOM(file);
     return new CompanionSpecification(dom.window.document);
 }
@@ -27,19 +27,19 @@ const setup_converter = async (target: string) => {
 
 describe("Test extraction of extension tags and writing them to the according sections", () => {
     test("Find all extension tags associated to a certain node", async () => {
-        const converter = await setup_converter("http://weihenstephan-standards.com/WS/");
+        const converter = await setup_converter("http://opcfoundation.org/UA/WeihenstephanStandards/WSBasis/");
         const nodes = converter.get_traced_nodes()
-        const target_node = nodes.filter((inode) => inode.browsename === "8:WSFormType").at(0);
+        const target_node = nodes.filter((inode) => inode.browsename === "10:WSFormType").at(0);
         expect(target_node).toBeDefined();
         if (target_node) {
             expect(target_node.implementation_notes.notes.length).toEqual(4)
         }
     });
-    //    test("Check for included extensions in the docx file", async () => {
-    //const converter = await setup_converter("http://weihenstephan-standards.com/WS/");
-    //const out = converter.write();
-    //await out.saveToFile("./", "test_extensions.docx");
-    //const file = fs.readFileSync("./test_extensions.docx");
-    //expect(file).toBeDefined();
-    //}, 0);
+    test("Check for included extensions in the docx file", async () => {
+        const converter = await setup_converter("http://opcfoundation.org/UA/WeihenstephanStandards/WSBasis/");
+        const out = converter.write();
+        await out.saveToFile("./", "test_extensions.docx");
+        const file = fs.readFileSync("./test_extensions.docx");
+        expect(file).toBeDefined();
+    }, 0);
 })
